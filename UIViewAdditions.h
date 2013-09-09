4317187cebeb96fcf9d4e0e39e0542d06edea10f
syang <syang@local>
2013-09-09 16:31:13 +0800

    request sina-weibo account access

---
+++ UIViewAdditions.h
@@ -0,0 +1,32 @@
+//
+//  UIViewAdditions.h
+//  TTUI
+//
+//  Created by shaohua on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (TTUI)
+
+@property (nonatomic) CGFloat left;
+@property (nonatomic) CGFloat top;
+@property (nonatomic) CGFloat right;
+@property (nonatomic) CGFloat bottom;
+@property (nonatomic) CGFloat width;
+@property (nonatomic) CGFloat height;
+
+@property (nonatomic) CGFloat centerX;
+@property (nonatomic) CGFloat centerY;
+
+@property (nonatomic) CGPoint origin;
+@property (nonatomic) CGSize size;
+
+@property (nonatomic, retain) UIColor *borderColor;
+
+// The view controller whose view contains this view.
+@property (nonatomic, readonly) UIViewController *viewController;
+
+- (BOOL)containsView:(UIView *)view;
+@end
